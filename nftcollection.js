e41fcdf8cab88259b82bc0ad9825824ba6ff1505
Yosu <114454865+jhnlgrys@users.noreply.github.com>
2024-05-27 20:50:34 +0800

    Update nftcollection.js

--- nftcollection.js
+++ nftcollection.js
@@ -1,20 +1,15 @@
-// Create a variable to hold a number of NFTs (Array is a suitable choice)
 let mintedNFTs = [];
 
-// mintNFT function to create and store NFT objects
 function mintNFT(name, description, imageURL) {
-  // Create an object to hold the NFT's metadata
   const nft = {
     name: name,
     description: description,
     imageURL: imageURL,
   };
 
-  // Add the newly minted NFT object to the mintedNFTs array
   mintedNFTs.push(nft);
 }
 
-// listNFTs function to iterate and print NFT metadata
 function listNFTs() {
   console.log("Your Minted NFTs:");
   for (const nft of mintedNFTs) {
@@ -25,12 +20,10 @@ function listNFTs() {
   }
 }
 
-// getTotalSupply function to return the number of minted NFTs
 function getTotalSupply() {
   return mintedNFTs.length;
 }
 
-// Example Usage
 mintNFT("Cool Cat", "A grumpy but adorable cat", "https://thisisanexample/cat.png");
 mintNFT("Space Traveler", "Lost but determined astronaut", "https://alsoanexample/astronaut.jpg");
 
